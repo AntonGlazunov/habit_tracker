8a648bfdb16ab53f0072e58be18ec9a3e2bfe2a5
Anton Glazunov <tagl91@yandex.ru>
2024-10-02 18:50:55 +0300

    add model way

---
+++ tracker/models.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Way(models.Model):
+    UNITS_MEASUREMENT_CHOICES = {
+        ('HH', 'Часы'),
+        ('MM', 'Минуты'),
+        ('SS', 'Секунды')
+    }
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь',
+                              related_name='way', **NULLABLE)
+    spot = models.CharField(max_length=200, verbose_name='Место')
+    data_time = models.DateTimeField(auto_now=False, verbose_name='Дата и время')
+    action = models.TextField(verbose_name='Действие')
+    is_nice_way = models.BooleanField(default=False, verbose_name='Приятная привычка')
+    associated_way = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='Связанная привычка', **NULLABLE)
+    periodicity = models.IntegerField(verbose_name='Периодичность в днях')
+    reward = models.CharField(max_length=200, verbose_name='Вознаграждение')
+    units_measurement = models.CharField(max_length=2, verbose_name='Еденицы измерения времени выполнения',
+                                         choices=UNITS_MEASUREMENT_CHOICES,
+                                         **NULLABLE)
+    time_execution = models.IntegerField(verbose_name='Время в выбраных еденицых измерения')
+    is_public = models.BooleanField(default=False, verbose_name='Публичная привычка')
+
+    def __str__(self):
+        return (f'{self.owner} {self.action} {self.is_nice_way} {self.associated_way} {self.periodicity} {self.reward} '
+                f'{self.units_measurement} {self.time_execution} {self.is_public}')
+
+    class Mets:
+        verbose_name = 'Привычка'
+        verbose_name_plural = 'Привычки'
